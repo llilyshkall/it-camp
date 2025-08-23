bf0c8bf6d998376b7a1c11138442fce31cd562f8
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2025-08-23 09:48:27 +0300

    parse excel func

--- services/remarks/cmd/remarks/main.go
+++ services/remarks/cmd/remarks/main.go
@@ -8,7 +8,9 @@ import (
 
 func main() {
 	http.HandleFunc("/health", httputils.HealthHandler)
+	http.HandleFunc("/load_excel_reestr", httputils.HealthHandler)
 	fmt.Println("remarks service running on :8080")
+	httputils.ParseExcel("../../internal/httputils/РЕЕСТР_ягодное - нулевой.xlsx")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
